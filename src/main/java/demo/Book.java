bc96f900caf976a8f402c584d64ff7d804737299
Deliwala, Alay <alay.deliwala@optum.com>
2018-07-17 13:57:21 -0400

    initial Commit

---
+++ src/main/java/demo/Book.java
@@ -0,0 +1,39 @@
+package demo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity // This tells Hibernate to make a table out of this class
+@Table(name="Book") // This will be the name of the table
+public class Book { 
+    @Id // Auto generated id number for the book
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+	@Column(nullable = false)
+    private String name;
+
+	public Book(){ } // Default Constructor
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
+
+
