fb08961eddef8791d50e54dd80769036f56c06e9
Deliwala, Alay <alay.deliwala@optum.com>
2018-07-18 09:19:10 -0400

    changed some documentation

--- src/main/java/demo/BookRepository.java
+++ src/main/java/demo/BookRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import demo.Book;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// This will be AUTO IMPLEMENTED by Spring into a Bean called bookRepository
 // CRUD refers Create, Read, Update, Delete
 
 public interface BookRepository extends CrudRepository<Book, Long> {
