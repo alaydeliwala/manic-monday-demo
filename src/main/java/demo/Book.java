3e8a3fe57d7768c00891d270b58612bfa5ef620f
Deliwala, Alay <alay.deliwala@optum.com>
2018-07-19 13:14:13 -0400

    added dao

--- src/main/java/demo/Book.java
+++ src/main/java/demo/Book.java
@@ -34,8 +34,3 @@ public class Book {
 		this.name = name;
 	}
 }
-
-
-
-
-
--- src/main/java/demo/BookDAO.java
+++ src/main/java/demo/BookDAO.java
@@ -1,13 +1,16 @@
 package demo;
 
+import javax.transaction.Transactional;
 import org.springframework.data.repository.CrudRepository;
-
+import org.springframework.stereotype.Repository;
 import demo.Book;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called bookRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface BookRepository extends CrudRepository<Book, Integer> {
+@Repository
+@Transactional
+public interface BookDAO extends CrudRepository<Book, Integer> {
 
 }
 
